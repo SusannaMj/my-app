f1a9ae86f076e69073e301e77e6475b788f1dfa4
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-01-26 23:52:56 +1000

    created basic app structure and styled css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useState from "react-usestateref";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" onChange={updateCity} />
+      <input type="submit" value="Search" />
+    </form>
   );
+
+  if (loaded === false) {
+    return (
+      <div>
+        {form}
+        <h1>
+          This project is <a href="/">open-sourced</a>
+        </h1>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {form}
+
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {Math.round(weather.wind)}km/h</li>
+          <li>
+            <img src={weather.icon} alt="Weather icon" />
+          </li>
+        </ul>
+        <h1>
+          This project is <a href="/">open-sourced</a>
+        </h1>
+      </div>
+    );
+  }
+
+  function displayWeather(response) {
+    setLoaded("true");
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9e215efc106c765d44690271c6c7636c&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
 }
 
 export default App;
